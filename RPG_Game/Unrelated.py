e309c1783510d1d779f065724e50bff8357d4945
A.E.Ugurlu@student.tudelft.nl <Ugurlu96>
2020-09-01 11:36:30 +0200

    Started a Flask project, will rollback the attempts at the camera here

--- RPG_Game/Unrelated.py
+++ RPG_Game/Unrelated.py
@@ -1,7 +1,16 @@
 import math
+listy = []
 
+for i in range(16):
+	listy.append(i)
 
-for i, j in enumerate(range(36)):
+for i, j in enumerate(listy):
 	print(i, math.factorial(j))
 
-print(100000//5)
+print('\n')
+print("Testing some numbers")
+
+numbers = [1, 4, 5, 9, 10, 11, 15]
+
+for num in numbers:
+	print(str(num) + '! has ' + str(num//5) + ' zeroes')
