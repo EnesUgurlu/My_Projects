12b41f2335dca699a1ea0cf2e7f3ef8e6a97f3a2
enesu <enesugurlu96@gmail.com>
2020-10-25 13:07:02 +0100

    Fixed the logic and cleaned up the code

--- Weapons_Calculator/Main.py
+++ Weapons_Calculator/Main.py
@@ -1,58 +1,58 @@
 # This is an attempt to make a calculator for weapons in Dragalia Lost
 
 def calculator():
-	# type_wep = input('Do you want to craft a HDT or Agito weapon? ') # Assume Agito
-	check_wep = input('Have you crafted the base weapon? ')
+	# type_wep = input('Do you want to craft a HDT or Agito weapon? ') # Assume Agito for now
+	Agito_weapons_first_4 = {
+		'Silver Masks': '40',
+		'Gold Masks': '30',
+		'Insanity': '0',
+		'Sand': '1',
+		'Orichalcum': '0',
+		'Rupies': '2000000'
+	}
 
+	Agito_weapons_last_4 = {
+		'Silver Masks': '16',
+		'Gold Masks': '10',
+		'Insanity': '7',
+		'Sand': '0',
+		'Orichalcum': '1',
+		'Rupies': '7500000'
+	}
+	result_dict = dict()
+	check_wep = input('Have you crafted the base weapon? ')
 	if check_wep.lower() == 'yes' or 'y':
+
 		curr_ub = int(input('How many unbinds does it have? '))
 		desired_ub = int(input('How many unbinds do you want to end at? '))
-		needed_ub = desired_ub - curr_ub
-		# Input the necessary materials here
-		one_to_four_silver_mask = (needed_ub - 4) * int(Agito_weapons_first_4['Silver Masks'])
-		five_to_eight_silver_mask = needed_ub * 16
-		refine_silver_mask = 16
+		silver_masks = 0
 
 		if curr_ub >= desired_ub:
 			print('You have more unbinds than you wanted!')
 			return
-		if needed_ub >= 4:
-			# Here we need the 5-8 unbind materials
-			print(f'{five_to_eight_silver_mask} Silver Masks needed for the last 4 unbinds')
-			print(f'{refine_silver_mask} Silver Masks needed for refinement')
 
-		total_silver_masks = one_to_four_silver_mask + five_to_eight_silver_mask + refine_silver_mask
-		print(f'{one_to_four_silver_mask} Silver Masks needed for the first 4 unbinds')
-		print(f'{total_silver_masks} Silver Masks needed total')
+		if 0 <= curr_ub <= 4:
+			needed_ub_first_half = 4 - curr_ub
+			silver_masks = needed_ub_first_half * int(Agito_weapons_first_4['Silver Masks'])
+			needed_ub_second_half = desired_ub - 4
+			silver_masks += needed_ub_second_half * int(Agito_weapons_last_4['Silver Masks'])
+			# Todo: Add refinement logic
 
+	result_dict['Silver Masks'] = silver_masks
+
+	return result_dict
 
 	# We need one more 'lower unbind' materials here, I will add it manually
-	elif check_wep.lower() == 'no' or 'n':
-		desired_ub = int(input('How many unbinds do you want to end at? '))
-		needed_ub = desired_ub + 1 # +1 lower_unbind
+	# elif check_wep.lower() == 'no' or 'n':
+	# 	desired_ub = int(input('How many unbinds do you want to end at? '))
+	# 	needed_ub = desired_ub + 1 # +1 lower_unbind
 
 
-calculator()
+print(calculator())
 
 
 
-Agito_weapons_first_4 = {
-	'Silver Masks': '40',
-	'Gold Masks': '30',
-	'Insanity': '0',
-	'Sand': '1',
-	'Orichalcum': '0',
-	'Rupies': '2000000'
-}
 
-Agito_weapons_last_4 = {
-	'Silver Masks': '40',
-	'Gold Masks': '30',
-	'Insanity': '0',
-	'Sand': '1',
-	'Orichalcum': '0',
-	'Rupies': '2000000'
-}
 
 # Materials for Agito:
 # Base weapon:
